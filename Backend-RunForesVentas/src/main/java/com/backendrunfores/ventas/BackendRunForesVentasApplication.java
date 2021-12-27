5d43f02c2f3f62bb7e0f7795d631ea80359094a6
David Forero <davidforero717@gmail.com>
2021-12-27 16:53:22 -0500

    se crea la clase Customer con su constructor y atributos se realiza la persistencia de JPA para crear la tabla customers y sus campos

---
+++ Backend-RunForesVentas/src/main/java/com/backendrunfores/ventas/BackendRunForesVentasApplication.java
@@ -0,0 +1,13 @@
+package com.backendrunfores.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRunForesVentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRunForesVentasApplication.class, args);
+    }
+
+}
---
+++ Backend-RunForesVentas/src/main/java/com/backendrunfores/ventas/models/entity/Customer.java
@@ -0,0 +1,105 @@
+package com.backendrunfores.ventas.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 10)
+    private Integer document;
+    @Column(nullable = false, length = 60)
+    private String name;
+    @Column(nullable = false, length = 60)
+    private String lastName;
+    @Column(nullable = true, length = 10)
+    private Integer phone;
+    @Column(nullable = true, length = 40)
+    private String email;
+
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getDocument() {
+        return document;
+    }
+
+    public void setDocument(Integer document) {
+        this.document = document;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+
+    @PrePersist
+    private void antesDePersistir(){
+        this.createAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    private void antesDeUpdate(){
+
+        this.updateAt = LocalDateTime.now();
+    }
+}
