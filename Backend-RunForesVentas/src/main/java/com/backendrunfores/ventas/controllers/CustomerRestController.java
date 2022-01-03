3a27a1e4679cc6db45b8df7a2d388279dc53ae14
David Forero <davidforero717@gmail.com>
2022-01-03 11:13:34 -0500

    se usa CrossOrigin o cor para compartir recursos en el Api rest

--- Backend-RunForesVentas/src/main/java/com/backendrunfores/ventas/controllers/CustomerRestController.java
+++ Backend-RunForesVentas/src/main/java/com/backendrunfores/ventas/controllers/CustomerRestController.java
@@ -3,12 +3,14 @@ package com.backendrunfores.ventas.controllers;
 import com.backendrunfores.ventas.models.entity.Customer;
 import com.backendrunfores.ventas.models.services.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
