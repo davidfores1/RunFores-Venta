53cc3c4a047947df17e42d8b3ed1a1c324acdd7e
David Forero <davidforero717@gmail.com>
2022-04-15 15:44:48 -0500

    se crea el repositorio JPA IusuarioDao y la clase servicio UsuarioService

---
+++ Backend-RunForesVentas/src/main/java/com/backendrunfores/ventas/models/services/UsuarioService.java
@@ -0,0 +1,50 @@
+package com.backendrunfores.ventas.models.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.backendrunfores.ventas.models.dao.IUsuarioDao;
+import com.backendrunfores.ventas.models.entity.Usuario;
+
+@Service
+public class UsuarioService implements UserDetailsService{
+	
+	private final Logger log = LoggerFactory.getLogger(UsuarioService.class);
+	
+	@Autowired
+	private IUsuarioDao usuarioDao;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		
+		Usuario usuario = usuarioDao.findByUsername(username);
+		
+		if(usuario == null) {
+		     log.error("Error en el login: no existe el usuario '"+ username+"' en el sistema!");	
+		     throw new UsernameNotFoundException("Error en el login: no existe el usuario '"+username+"' en el sistema!");
+		}
+		
+		List<GrantedAuthority> authorities = usuario.getRoles()
+				.stream()
+				.map(rol -> new SimpleGrantedAuthority(rol.getNombre()))
+				.peek(authority -> log.info("Rol: " + authority.getAuthority()))
+	            .collect(Collectors.toList()); 	
+		
+		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
+	}
+
+}
